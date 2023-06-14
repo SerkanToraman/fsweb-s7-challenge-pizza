a6eba91bdc80283c221fe195324b1add82949529
serkantoraman <serkan@github.com>
2023-06-14 08:26:53 +0300

    Main Page Responsive Imporvement Done

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styled from 'styled-components';
 import { Link } from "react-router-dom";
+
+
+
 import '../css/MainPage.css'
 
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./reset.css"
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.render(
 <BrowserRouter>
